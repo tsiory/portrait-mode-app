0ada00de50e63b66fc3145b660d53f28888a03ba
Tsiory <tsiory.anhykrishna@novity.io>
2022-06-03 15:19:47 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    function lock(orientation: OrientationLockType) {
+      console.log('Lock called', orientation);
+
+      let de = document.documentElement;
+      if (de.requestFullscreen) {de.requestFullscreen()}
+
+      window.screen.orientation.lock(orientation);
+      alert('Orientation locked: ' + orientation);
+    }
+
+    lock('portrait');
+  }, []);
+
   return (
     <div className="App">
       Hello world
