28a7ff47ab917deefaedc804697ce5bfa079daa3
Tsiory <tsiory.anhykrishna@novity.io>
2022-06-03 15:30:06 +0300

    update oh

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,14 @@ function App() {
     function lock(orientation: OrientationLockType) {
       console.log('Lock called', orientation);
 
-      let de = document.documentElement;
+      let de = document.documentElement as any;
       if (de.requestFullscreen) {de.requestFullscreen()}
+      else if (de.mozRequestFullscreen) {de.mozRequestFullscreen()}
+      else if (de.webkitRequestFullscreen) {de.webkitRequestFullscreen()}
+      else if (de.msRequestFullscreen) {de.msRequestFullscreen()}
 
       window.screen.orientation.lock(orientation);
-      alert('Orientation locked: ' + orientation);
+      alert('update oh');
     }
 
     lock('portrait');
